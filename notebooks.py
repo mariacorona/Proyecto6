9e6be627f0c1d74d03ce482e4bedb3485a9b22b4
mariacorona <marifer9734@gmail.com>
2024-09-05 20:18:43 -0600

    creacion de grafico de dispersion  en el server web

--- notebooks.py
+++ notebooks.py
@@ -1,6 +1,23 @@
 import pandas as pd
-import plotly.express as px
+import streamlit as st 
+import plotly.express as px 
+
+datos= pd.read_csv('vehicles_us.csv')
+
+#print(datos.head())
+
+hist_boton = st.button("Construir histograma")
+
+if hist_boton :
+      
+    st.write("Creacion de un histograma de los valores del odometro de los autos")
+      
+    fig = px.histogram(datos, x= "odometer")
+      
+    st.plotly_chart(fig)
+      
+
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-fig = px.histogram(car_data, x="odometer") # crear un histograma
-fig.show() # crear gráfico de dispersión 
+fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+fig.show() # crear gráfico de dispersión

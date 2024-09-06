d8cab0a1ade615a15b0f66a296b6fb12099dc26e
mariacorona <marifer9734@gmail.com>
2024-09-05 23:01:04 -0600

    agregar casillas

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import pandas as pd
 import streamlit as st 
 import plotly.express as px 
 
+datos= pd.read_csv('vehicles_us.csv')
+
+#print(datos.head())
+
+
 disp_boton = st.button("Construir gráfico de dispersión")
 
 if disp_boton :
@@ -12,4 +17,14 @@ if disp_boton :
     fig_disp = px.scatter(datos, x="odometer", y="price") # crear un gráfico de dispersión
       
     st.plotly_chart(fig_disp) # crear gráfico de dispersión
-    
+
+
+hist_boton = st.button("Construir histograma")
+
+if hist_boton :
+      
+    st.write("Creacion de un histograma de los valores del odometro de los autos")
+      
+    fig = px.histogram(datos, x= "odometer")
+      
+    st.plotly_chart(fig)    
